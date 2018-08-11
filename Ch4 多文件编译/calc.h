ff0d4868e0c3c5884fb3035112f2d48ee70e5b86
LYJSher <40586824+LYJSher@users.noreply.github.com>
2018-08-11 17:39:11 +0800

    Ch4 4.9

---
+++ Ch4 多文件编译/calc.h
@@ -0,0 +1,6 @@
+# define NUMBER '0' // 标识找到一个数
+int getop(char []);
+void push(double);
+double pop(void);
+int getch(void); // 读入下一个待处理的字符
+void ungetch(int); // 把字符放回到输入中
---
+++ Ch4 多文件编译/getch.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# define BUFSIZE 100
+char buf[BUFSIZE]; // 用于ungetch()函数的缓冲区
+int bufp = 0; // buf中下一个空闲位置
+
+// 取一个字符（可能是压回的字符）
+int getch(void){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+// 把字符压回输入中
+void ungetch(int c){
+    if(bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
